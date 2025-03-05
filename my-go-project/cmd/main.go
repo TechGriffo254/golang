96e53c3708f6559ee1bac99fdcefbd8f9757d33f
TechGriffo254 <griffotech254@gmail.com>
2025-03-05 11:52:42 +0300

    updated main.go

--- my-go-project/cmd/main.go
+++ my-go-project/cmd/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-    fmt.Println("Starting the application...")
+	fmt.Println("hello world ..")
 
-    // Define your routes and handlers here
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, World!")
-    })
+	// Define your routes and handlers here
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
 
-    // Start the server
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	// Start the server
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
